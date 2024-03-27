63df3accc5501c8a06afff2dad0913b146016eca
Julian Magallanes <julianmagallanes1994@gmail.com>
2024-03-27 18:54:33 -0300

    agregando links

--- front/src/components/MainHome/MainHome.jsx
+++ front/src/components/MainHome/MainHome.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import style from "./MainHome.module.css"
 import imageProfile from "../../assets/icon.png"
 const MainHome = () => {
+    const [deploy, setDeploy] = useState(false)
+
+    const handleDeploy = () => {
+        if(deploy===false){
+            setDeploy(true)
+        }else(
+            setDeploy(false) 
+        )
+    }
+    const handleDismantle = () => {
+        setDeploy(false)
+    }
     return(
     <div className={style.mainHome_Container}>
         <div className={style.mainHome_ContainerImage}>
@@ -14,12 +26,16 @@ const MainHome = () => {
                 <p>Soy full Stack Developer, especializado en el Frontend de las aplicaciones web. Me gusta el trabajo en equipo, creo que los proyectos en conjunto llegan a mejores resultados.</p>
             </div>
             <div className={style.mainHome_ContainerButton}>
-                <button className={style.buttonDark2}>+</button>
-                <button className={style.buttonDark}>GitHub</button>
-                <button className={style.buttonLigth}>Linkedln</button>
-                <button className={style.buttonLigth}>Curriculum</button>
-                <button className={style.buttonDark}>Trabajos</button>
-                <button className={style.buttonLigth2}>-</button>
+                <button className={style.buttonDark2} onClick={handleDeploy}>+</button>
+                {deploy === true &&
+                <>
+                <a href="https://github.com/Julian-Magallanes" rel="noopener" target="_blank" className={style.buttonDark}>GitHub</a>
+                <a href="https://www.linkedin.com/in/juli%C3%A1n-magallanes-2b074a187/" rel="noopener" target="_blank" className={style.buttonLigth}>Linkedln</a>
+                <a href="https://drive.google.com/file/d/102D9Xdrinpob5NAu2kn2oyWXw4ePZCsG/view?usp=drive_link" rel="noopener" target="_blank" className={style.buttonLigth}>Curriculum</a>
+                <a href="https://wa.me/542613012708" rel="noopener" target="_blank" className={style.buttonDark}>Whatsapp</a>
+                <button className={style.buttonLigth2} onClick={handleDismantle}>-</button>
+                </>
+                }
             </div>
         </div>
     </div>
--- front/src/views/Education/Education.jsx
+++ front/src/views/Education/Education.jsx
@@ -1,49 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
 import style from "./Education.module.css"
 
 const Education = () => {
+    const [deploy, setDeploy] = useState(false)
+
+    const handleDeploy = () => {
+        if(deploy===false){
+            setDeploy(true)
+        }else(
+            setDeploy(false) 
+        )
+    }
+    const handleDismantle = () => {
+        setDeploy(false)
+    }
     return(
     <div className={style.educationContainer} id="Education">
         <div className={style.education}>
-            <div className={style.educationCardLeft}>
+            <div className={style.educationCardLeft} id="universidad">
                 <h4>
                     Universidad de Mendoza
                 </h4>
                 <h6>
-                    <i>Arquitecto, Arquitectura - 2014 -2020</i>
+                    <div><i>Arquitecto</i></div>
+                    Arquitectura - 2014 -2020
                 </h6>
-                <button>CERTIFICADO</button>
+                <a href="https://drive.google.com/file/d/1PNFQmmSWNPXKzuHfLk1CR2h99KsmdaXR/view?usp=sharing" rel="noopener" target="_blank">CERTIFICADO</a>
             </div>
-            <div className={style.educationCardLeft}>
+            <div className={style.educationCardLeft} id="coderhause">
                 <h4>
                     CURSO DE CODERHOUSE
                 </h4>
                 <h6> 
-                    <i>Carrera de Desarrollo Web</i>
+                    <div><i>Carrera de Desarrollo Web</i></div>
                     HTML - CSS - GIT - SASS -Bootstrap Javascript - React
                 </h6>
-                <button>CERTIFICADO</button>
+                <div className={style.educationDeploy}>
+                    <button onClick={handleDeploy}>+</button>
+                    {deploy === true &&
+                    <>
+                        <a href="https://drive.google.com/file/d/1vfjffV3od27AQp1y_gEpnCjWfPrE4XJS/view?usp=sharing" rel="noopener" target="_blank">DESARROLLO WEB</a>
+                        <a href="https://drive.google.com/file/d/13xv-kvym_ydEubwuUGQgM8UpAKKuo02b/view?usp=sharing" rel="noopener" target="_blank">JAVASCRIPT</a>
+                        <a href="https://drive.google.com/file/d/1mHpnCM0JlPFeAPc_KJvE-VNbMCN126aI/view?usp=sharing" rel="noopener" target="_blank">REACT JS</a>
+                        <a href="https://drive.google.com/file/d/1hraX7J71UO1HA3q0VW2RLuw3e2vEirQy/view?usp=sharing" rel="noopener" target="_blank">FRONTEND</a>
+                        <button onClick={handleDismantle}>-</button>
+                    </>}
+                </div>
             </div>
-            <div className={style.educationCardRigth}>
+            <div className={style.educationCardRigth} id="henry">
                 <h4>
                     BOOTCAMP - SOY HENRY
                 </h4>
                 <h6> 
-                    <i>FullStack Developer</i>
+                    <div><i>FullStack Developer</i></div>
                     Javascript - Tipescript- React.js
                     Node.js - Mongo DB - Progrest 
                     -Next.js - Nest/js 
                 </h6>
-                <button>CERTIFICADO</button>
+                <a href="./" rel="noopener" target="_blank">CERTIFICADO</a>
             </div>
-            <div className={style.educationCardLeft}>
+            <div className={style.educationCardLeft} id="autodidacta">
                 <h4>
                     AUTODIDACTA
                 </h4>
                 <h6> 
                     Amazon Web Services 
                 </h6>
-                <button>CERTIFICADO</button>
             </div>
         </div>
     </div>
